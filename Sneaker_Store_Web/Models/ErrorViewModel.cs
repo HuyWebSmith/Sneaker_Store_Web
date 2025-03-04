bf901a81fd700f01c1c54b9bfc8de2e9cb4443e3
Quang Huy coder <n.quanghuy2104@gmail.com>
2025-03-04 23:57:07 +0700

    Add project files.

---
+++ Sneaker_Store_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sneaker_Store_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
